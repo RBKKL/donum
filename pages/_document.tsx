4b750ada86ef401153d8f0bd67be5a610c52df3f
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-10-01 08:51:04 +0700

    add _document.tsx

---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+const Document = () => (
+  <Html>
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
+        rel="stylesheet"
+      />
+    </Head>
+    <body className="bg-zinc-900 text-white">
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+);
+
+export default Document;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ const Home: NextPage = () => {
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
         <title>Donum</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
