37ec0ce6fea56527c90389def53ce79ef0fa5dbb
Nikita Latysh <n.latysh@g.nsu.ru>
2022-10-03 19:16:37 +0700

    Added donate page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      spacing: {
+        'anti-2/4': '-50%',
+        '50': '12.5rem',
+        '128': '32rem',
+        'anti-0.5': '-0.125rem',
+      },
+    },
+    borderWidth: {
+      '3': '3px',
+    },
+    borderRadius: {
+      '15': '15px',
+      '30': '30px',
+    },
+    fontFamily: {
+      'inter': ['Inter', 'sans-serif'],
+      'jeju-gothic': ['jeju-gothic', 'sans-serif'],
+    }
   },
   plugins: [],
 }
