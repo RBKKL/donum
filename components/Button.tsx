c40d7aeaa642d1e24e14766e11eb8f542909c029
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-10-13 12:39:51 +0700

    Fix button layout

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC } from "react";
 
 interface ButtonProps {
   disabled?: boolean;
@@ -8,10 +8,10 @@ interface ButtonProps {
 
 export const Button: FC<ButtonProps> = ({ disabled, onClick, text }) => (
   <button
-    className="rounded-full bg-yellow-500 px-8 py-2 w-full sm:w-fit"
+    className="rounded-full bg-yellow-500 px-8 py-2 w-full sm:w-fit font-semibold text-xl"
     disabled={disabled}
     onClick={onClick}
   >
-    <p className="font-semibold text-xl">{text}</p>
+    {text}
   </button>
 );
