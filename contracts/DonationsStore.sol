ef9056ca6a93fbdccb7f5759464a45aab0d89652
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-10-15 22:21:48 +0700

    upgrade contract to use event for donation storage

--- contracts/DonationsStore.sol
+++ contracts/DonationsStore.sol
@@ -2,50 +2,28 @@
 pragma solidity ^0.8.0;
 
 contract DonationsStore {
-    struct Donation {
-        address from;
-        uint amount;
-        uint timestamp;
-        string message;
-    }
-
     struct RecipientInfo {
         string nickname;
         string avatarURI;
-        Donation[] donations;
     }
 
-    // data is same as in Donation struct
     event NewDonation(
         address from,
+        address to,
         uint amount,
         uint timestamp,
         string message
     );
 
-    // TODO: change to logs for gas saving? https://consensys.net/blog/developers/guide-to-events-and-logs-in-ethereum-smart-contracts/
-    mapping(address => RecipientInfo) recipients;
+    mapping(address => RecipientInfo) public recipients;
 
-    function getAllDonations(address _recipient) external view returns (Donation[] memory) {
-        return recipients[_recipient].donations;
-    }
-
-    function getDonation(address _recipient, uint _index) external view returns (Donation memory) {
-        return recipients[_recipient].donations[_index];
-    }
-
-    function getInfo(address _recipient) external view returns (string memory, string memory) {
-        return (recipients[_recipient].nickname, recipients[_recipient].avatarURI);
-    }
-
-    function setInfo(string calldata _nickname, string calldata _avatarURI) external {
+    function setRecipientInfo(string calldata _nickname, string calldata _avatarURI) external {
         recipients[msg.sender].nickname = _nickname;
         recipients[msg.sender].avatarURI = _avatarURI;
     }
 
     function donate(address _to, string calldata _message) external payable {
-        recipients[_to].donations.push(Donation(msg.sender, msg.value, block.timestamp, _message));
         payable(_to).transfer(msg.value);
-        emit NewDonation(msg.sender, msg.value, block.timestamp, _message);
+        emit NewDonation(msg.sender, _to, msg.value, block.timestamp, _message);
     }
 }
