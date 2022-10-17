d16e062ba491ed03a3c9fde6cfd52fe7627b4938
ZdarovaMujiki <vvremezovv@mail.ru>
2022-10-17 11:02:29 +0700

    Add disabled button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 export const Button: FC<ButtonProps> = ({ disabled, onClick, text }) => (
   <button
-    className="rounded-full bg-yellow-500 px-8 py-2 w-full sm:w-fit font-semibold text-xl"
+    className="rounded-full bg-yellow-500 px-8 py-2 w-full sm:w-fit font-semibold text-xl disabled:opacity-50"
     disabled={disabled}
     onClick={onClick}
   >
