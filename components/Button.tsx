08e647b023aaf6fc897e35a5288c651490e4e0d9
ZdarovaMujiki <vvremezovv@mail.ru>
2022-10-17 11:07:21 +0700

    Fix disabled button style

--- components/Button.tsx
+++ components/Button.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
 
 export const Button: FC<ButtonProps> = ({ disabled, onClick, text }) => (
   <button
-    className="rounded-full bg-yellow-500 px-8 py-2 w-full sm:w-fit font-semibold text-xl disabled:opacity-50"
+    className="rounded-full bg-yellow-500 px-8 py-2 w-full sm:w-fit font-semibold text-xl disabled:opacity-50 transition-opacity"
     disabled={disabled}
     onClick={onClick}
   >
