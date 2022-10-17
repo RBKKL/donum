5473e9cb1c575db16424c8b4c006d0f7e39205c4
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-10-17 13:01:41 +0700

    Change names

--- components/DonationAlert.tsx
+++ components/DonationAlert.tsx
@@ -1,23 +1,24 @@
 import { FC } from "react";
 import { ResponsiveImage } from "@components/ResponsiveImage";
+import { BigNumber, ethers } from "ethers";
 
-interface AlertProps {
-  imgSrc: string;
-  donateValue: number;
-  senderAddress: string;
+interface DonationAlertProps {
+  src: string;
+  amount: BigNumber;
+  sender: string;
   message: string;
 }
 
-export const Alert: FC<AlertProps> = ({
-  imgSrc,
-  donateValue,
-  senderAddress,
+export const DonationAlert: FC<DonationAlertProps> = ({
+  src,
+  amount,
+  sender,
   message,
 }) => (
   <div className="w-full h-full flex flex-col items-center bg-green-screen">
-    <ResponsiveImage src={imgSrc} />
+    <ResponsiveImage src={src} />
     <h1 className="text-center pt-2.5 text-yellow-500 text-3xl font-bold text-border-2">
-      {senderAddress} sent {donateValue} ETH
+      {sender} sent {ethers.utils.formatEther(amount)} ETH
     </h1>
     <p className="text-center pt-2.5 text-zinc-50 text-border-1 leading-none text-xl">
       {message}
--- pages/alert/[address].tsx
+++ pages/alert/[address].tsx
@@ -1,11 +1,12 @@
 import { NextPage } from "next";
-import { Alert } from "@components/Alert";
+import { DonationAlert } from "@components/DonationAlert";
+import { ethers } from "ethers";
 
 const AlertPage: NextPage = () => (
-  <Alert
-    imgSrc="/assets/images/default_avatar.gif"
-    donateValue={0.001}
-    senderAddress="0x5F...0aa3"
+  <DonationAlert
+    src="/assets/images/default_avatar.gif"
+    amount={ethers.utils.parseEther("0.001")}
+    sender="0x5F...0aa3"
     message="100 процентов людей, сидящих в этом зале, не знают, кто я такой. И это хорошо. Это правильно. Год назад я жил на окраине Академа. В крохотном общежитии с тараканами, клопами и одинокой мышью в духовке, а моим соседом был татуировщик Саня из Северобайкальск"
   />
 );
