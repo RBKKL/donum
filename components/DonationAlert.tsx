ee616e85aea9e543e6f2a62f01cae8b97c9fa213
Strafred <bred7355608@gmail.com>
2022-10-18 12:57:09 +0700

    trying to implement donations timeouts

--- components/DonationAlert.tsx
+++ components/DonationAlert.tsx
@@ -4,7 +4,7 @@ import { BigNumber, ethers } from "ethers";
 
 interface DonationAlertProps {
   src: string;
-  amount: BigNumber;
+  amount: string;
   sender: string;
   message: string;
 }
@@ -18,10 +18,11 @@ export const DonationAlert: FC<DonationAlertProps> = ({
   <div className="w-full h-full flex flex-col items-center bg-green-screen">
     <ResponsiveImage src={src} />
     <h1 className="text-center pt-2.5 text-yellow-500 text-3xl font-bold text-border-2">
-      {sender} sent {ethers.utils.formatEther(amount)} ETH
+      {sender} sent {amount} ETH
     </h1>
     <p className="text-center pt-2.5 text-zinc-50 text-border-1 leading-none text-xl">
       {message}
+        <audio id='a1' hidden src="http://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3"/>
     </p>
   </div>
 );
--- pages/alert/[address].tsx
+++ pages/alert/[address].tsx
@@ -1,14 +1,91 @@
-import { NextPage } from "next";
-import { DonationAlert } from "@components/DonationAlert";
-import { ethers } from "ethers";
+import {NextPage} from "next";
+import {DonationAlert} from "@components/DonationAlert";
+import {ethers} from "ethers";
+import {useRouter} from "next/router";
+import {useContractEvent, useNetwork} from "wagmi";
+import {useEffect, useState} from "react";
+import {CONTRACT_ABI, getContractAddressByChainId} from "@lib/smartContractsData";
 
-const AlertPage: NextPage = () => (
-  <DonationAlert
-    src="/assets/images/default_avatar.gif"
-    amount={ethers.utils.parseEther("0.001")}
-    sender="0x5F...0aa3"
-    message="100 процентов людей, сидящих в этом зале, не знают, кто я такой. И это хорошо. Это правильно. Год назад я жил на окраине Академа. В крохотном общежитии с тараканами, клопами и одинокой мышью в духовке, а моим соседом был татуировщик Саня из Северобайкальск"
-  />
-);
+type Donation = {
+  from: string;
+  amount: string;
+  message: string;
+};
+
+const useAudio = () => {
+  const [playing, setPlaying] = useState(false);
+
+  const toggle = (isPlaying: boolean) => {
+    setPlaying(isPlaying);
+  }
+
+  useEffect(() => {
+    const audio = document.getElementById('a1') as HTMLVideoElement | null;
+    if (audio) {
+      playing ? audio.play() : audio.pause();
+    }
+  }, [playing]);
+
+  return toggle;
+};
+
+const AlertPage: NextPage = () => {
+  const [donationsQueue, setDonationsQueue] = useState<Donation[]>([]);
+  const toggle = useAudio();
+
+  useEffect(() => {
+    if (donationsQueue.length === 0) {
+      return;
+    }
+    toggle(true);
+
+    setTimeout(() => {
+      toggle(false);
+      popDonation();
+    }, 5000);
+  }, [donationsQueue]);
+
+  const pushDonation = (donation: Donation) => {
+    setDonationsQueue((prev) => [...prev, donation]);
+  }
+
+  const popDonation = () => {
+    const [, ...donations] = donationsQueue;
+    setDonationsQueue(donations);
+  }
+
+  const {chain} = useNetwork();
+  const router = useRouter();
+  const recipientAddress = router.query.address as string;
+
+  useContractEvent({
+    addressOrName: getContractAddressByChainId(chain?.id),
+    contractInterface: CONTRACT_ABI,
+    eventName: 'NewDonation',
+    listener(event) {
+      if (recipientAddress == event[1]) {
+        const donation: Donation = {
+          from: event[0],
+          amount: ethers.utils.formatEther(event[2]),
+          message: event[4]
+        }
+        pushDonation(donation);
+      }
+    },
+  });
+
+  const donation = donationsQueue[0];
+
+  if (donation)
+    return (
+        <DonationAlert
+            src="/assets/images/default_avatar.gif"
+            amount={donation?.amount}
+            sender={donation?.from}
+            message={donation?.message}/>
+    ); else {
+    return (<div className="w-full h-full bg-green-screen"/>);
+  }
+};
 
 export default AlertPage;
