ef91053d86d492b77def348269abae5cc1c2e7e5
ZdarovaMujiki <vvremezovv@mail.ru>
2022-11-06 19:34:54 +0700

    delete bad file

--- server/routers/_app.ts
+++ server/routers/_app.ts
@@ -1,10 +1,8 @@
 import { router } from "@server/trpc";
 import { profileRouter } from "./profile";
-import { donationPageRouter } from "./donationPage";
 
 export const appRouter = router({
   profile: profileRouter,
-  donationPage: donationPageRouter,
 });
 
 // export type definition of API
