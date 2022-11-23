35de68a438172d73fb84d2c9e644300c4f1d2bc9
Alexandr Baranov <baranovau216@gmail.com>
2022-11-23 07:20:40 +0700

    fix foramt

--- apps/contracts/abi/DonationsStoreABI.ts
+++ apps/contracts/abi/DonationsStoreABI.ts
@@ -1,99 +1,99 @@
 export const DonationsStoreABI = [
   {
-    "anonymous": false,
-    "inputs": [
+    anonymous: false,
+    inputs: [
       {
-        "indexed": false,
-        "internalType": "address",
-        "name": "from",
-        "type": "address"
+        indexed: false,
+        internalType: "address",
+        name: "from",
+        type: "address",
       },
       {
-        "indexed": true,
-        "internalType": "address",
-        "name": "to",
-        "type": "address"
+        indexed: true,
+        internalType: "address",
+        name: "to",
+        type: "address",
       },
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "amount",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "amount",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "uint256",
-        "name": "timestamp",
-        "type": "uint256"
+        indexed: false,
+        internalType: "uint256",
+        name: "timestamp",
+        type: "uint256",
       },
       {
-        "indexed": false,
-        "internalType": "string",
-        "name": "message",
-        "type": "string"
-      }
+        indexed: false,
+        internalType: "string",
+        name: "message",
+        type: "string",
+      },
     ],
-    "name": "NewDonation",
-    "type": "event"
+    name: "NewDonation",
+    type: "event",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "_to",
-        "type": "address"
+        internalType: "address",
+        name: "_to",
+        type: "address",
       },
       {
-        "internalType": "string",
-        "name": "_message",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "_message",
+        type: "string",
+      },
     ],
-    "name": "donate",
-    "outputs": [],
-    "stateMutability": "payable",
-    "type": "function"
+    name: "donate",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "address",
-        "name": "",
-        "type": "address"
-      }
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
     ],
-    "name": "recipients",
-    "outputs": [
+    name: "recipients",
+    outputs: [
       {
-        "internalType": "string",
-        "name": "nickname",
-        "type": "string"
+        internalType: "string",
+        name: "nickname",
+        type: "string",
       },
       {
-        "internalType": "string",
-        "name": "avatarURI",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "avatarURI",
+        type: "string",
+      },
     ],
-    "stateMutability": "view",
-    "type": "function"
+    stateMutability: "view",
+    type: "function",
   },
   {
-    "inputs": [
+    inputs: [
       {
-        "internalType": "string",
-        "name": "_nickname",
-        "type": "string"
+        internalType: "string",
+        name: "_nickname",
+        type: "string",
       },
       {
-        "internalType": "string",
-        "name": "_avatarURI",
-        "type": "string"
-      }
+        internalType: "string",
+        name: "_avatarURI",
+        type: "string",
+      },
     ],
-    "name": "setRecipientInfo",
-    "outputs": [],
-    "stateMutability": "nonpayable",
-    "type": "function"
-  }
+    name: "setRecipientInfo",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
 ] as const;
