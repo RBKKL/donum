f31dc39162703998d68c5b095d4d3ab412c1bd23
Nikita Latysh <n.latysh@g.nsu.ru>
2022-11-24 14:17:31 +0700

    add tailwind styles and props to loader component

--- apps/web/components/Loader.tsx
+++ apps/web/components/Loader.tsx
@@ -1,15 +1,148 @@
 import { FC } from "react";
 import styles from "@styles/loader.module.css";
 
-export const Loader: FC = () => (
-  <div className={styles.loader}>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
-    <div></div>
+interface LoaderProps {
+  size?: number;
+  color?: string;
+}
+
+export const Loader: FC<LoaderProps> = ({ size = 80, color = "white" }) => (
+  <div style={{ width: size, height: size }} className="relative inline-block">
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-1 origin-[]"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.7875 * size,
+          left: 0.7875 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-2"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.85 * size,
+          left: 0.7 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-3"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.8875 * size,
+          left: 0.6 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-4"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.9 * size,
+          left: 0.5 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-5"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.8875 * size,
+          left: 0.4 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-6"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.85 * size,
+          left: 0.3 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-7"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.7875 * size,
+          left: 0.2125 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
+    <div
+      style={{ transformOrigin: `${0.5 * size}px ${0.5 * size}px` }}
+      className="animate-loader-spin-8"
+    >
+      <div
+        style={{
+          width: 0.0875 * size,
+          height: 0.0875 * size,
+          backgroundColor: color,
+          marginTop: -0.05 * size,
+          marginLeft: -0.05 * size,
+          top: 0.7 * size,
+          left: 0.15 * size,
+        }}
+        className="absolute block rounded-full"
+      ></div>
+    </div>
   </div>
 );
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -14,6 +14,24 @@ module.exports = {
       fontFamily: {
         inter: ["var(--font-inter)", ...fontFamily.sans],
       },
+      animation: {
+        "loader-spin-1":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -36ms infinite",
+        "loader-spin-2":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -72ms infinite",
+        "loader-spin-3":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -108ms infinite",
+        "loader-spin-4":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -144ms infinite",
+        "loader-spin-5":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -180ms infinite",
+        "loader-spin-6":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -216ms infinite",
+        "loader-spin-7":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -252ms infinite",
+        "loader-spin-8":
+          "spin 1.2s cubic-bezier(0.5, 0, 0.5, 1) -288ms infinite",
+      },
     },
   },
   plugins: [],
