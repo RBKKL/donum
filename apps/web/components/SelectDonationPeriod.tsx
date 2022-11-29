7aff2d72084daa60386d9c985c2bc6a4a10824c7
Nikita Latysh <n.latysh@g.nsu.ru>
2022-11-30 02:16:52 +0700

    Add select library and work with it

--- apps/web/components/SelectDonationPeriod.tsx
+++ apps/web/components/SelectDonationPeriod.tsx
@@ -1,5 +1,6 @@
-import classNames from "classnames";
 import { Dispatch, FC, SetStateAction } from "react";
+import * as Select from "@radix-ui/react-select";
+import { ArrowIcon } from "./icons/ArrowIcon";
 
 export interface SelectOption {
   value: number;
@@ -20,23 +21,33 @@ export const SelectDonationPeriod: FC<SelectDonationPeriodProps> = ({
   className = "",
 }) => {
   return (
-    <select
-      value={selected}
-      onChange={(event) => onSelect(+event.target.value)}
-      className={classNames(
-        "bg-zinc-900 text-2xl font-semibold focus:outline-none",
-        className
-      )}
-    >
-      {options.map((option) => (
-        <option
-          className="font-semibold"
-          key={option.value}
-          value={option.value}
-        >
-          {option.text}
-        </option>
-      ))}
-    </select>
+    <div className={className}>
+      <Select.Root
+        value={"" + selected}
+        onValueChange={(newValue) => onSelect(+newValue)}
+      >
+        <Select.Trigger className="flex items-center gap-4 rounded-xl border border-yellow-500 px-2 py-1 text-2xl text-yellow-500 focus:outline-none">
+          <Select.Value>
+            {options.find((option) => option.value === selected)!.text}
+          </Select.Value>
+          <ArrowIcon />
+        </Select.Trigger>
+
+        <Select.Portal>
+          <Select.Content className="rounded-xl border border-yellow-500 bg-zinc-900 px-3 py-2">
+            <Select.Viewport>
+              {options.map((option) => (
+                <Select.Item
+                  className="cursor-pointer text-2xl hover:text-yellow-500 hover:outline-none focus:outline-none"
+                  value={"" + option.value}
+                >
+                  <Select.ItemText>{option.text}</Select.ItemText>
+                </Select.Item>
+              ))}
+            </Select.Viewport>
+          </Select.Content>
+        </Select.Portal>
+      </Select.Root>
+    </div>
   );
 };
