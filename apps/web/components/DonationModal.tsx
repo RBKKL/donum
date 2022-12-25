8e1a2831cd223d38b208abe9a848edf264c4255b
Strafred <bred7355608@gmail.com>
2022-12-25 21:55:03 +0700

    fix donation amount show

--- apps/web/components/DonationModal.tsx
+++ apps/web/components/DonationModal.tsx
@@ -34,7 +34,7 @@ export const DonationModal: FC<DonationModalProps> = ({
         <div className="flex w-full flex-col items-center">
           <p className="mb-1 text-lg font-semibold">Waiting for confirmation</p>
           <p className="mb-2 text-center text-lg font-semibold">
-            Donating {parseFloat(donationAmount)} ETH to {nickname}
+            Donating {Number(donationAmount).toFixed(8)} ETH to {nickname}
           </p>
           <p className="mb-2 text-xs">
             Confirm this transaction in your wallet
