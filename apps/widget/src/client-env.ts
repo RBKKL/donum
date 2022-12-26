33594d392565c0bd2ee6fb264dfdb63c464e3770
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-12-26 11:47:57 +0700

    fix widget env vars

--- apps/widget/src/client-env.ts
+++ apps/widget/src/client-env.ts
@@ -1,3 +1,4 @@
 export const clientEnv = {
-  EVENTS_SERVER_URL: process.env.EVENTS_SERVER_URL || "http://localhost:8000",
+  EVENTS_SERVER_URL:
+    import.meta.env.EVENTS_SERVER_URL || "http://localhost:8000",
 } as const;
