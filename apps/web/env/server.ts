12162e261900f087d34c1e206d99e94954a0e947
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-12-26 11:29:21 +0700

    fix webapp url for prod

--- apps/web/env/server.ts
+++ apps/web/env/server.ts
@@ -1,12 +1,14 @@
 export const serverEnv = {
-  VERCEL_URL: process.env.VERCEL_URL ?? "",
+  VERCEL_URL: process.env.VERCEL_URL ?? undefined,
   PORT: process.env.PORT ?? "3000",
   NODE_ENV: process.env.NODE_ENV ?? "development",
   SUPABASE_STORAGE_URL: process.env.SUPABASE_STORAGE_URL ?? "",
   SUPABASE_SERVICE_KEY: process.env.SUPABASE_SERVICE_KEY ?? "",
   NEXTAUTH_URL: process.env.NEXTAUTH_URL ?? "",
   NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET ?? "",
-  WEBAPP_BASE_URL: process.env.WEBAPP_BASE_URL ?? "",
+  WEBAPP_BASE_URL: process.env.VERCEL_URL
+    ? process.env.VERCEL_URL
+    : process.env.WEBAPP_BASE_URL ?? "",
   DATABASE_URL: process.env.DATABASE_URL ?? "",
   EVENTS_SERVER_URL: process.env.EVENTS_SERVER_URL ?? "",
   EVENTS_SERVER_AUTH_TOKEN: process.env.EVENTS_SERVER_AUTH_TOKEN ?? "",
