eb909ea2a34abf1d42235b8c19eeb78388cb4d24
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2022-12-29 02:34:58 +0700

    try fix prod

--- apps/web/pages/api/trpc/[trpc].ts
+++ apps/web/pages/api/trpc/[trpc].ts
@@ -1,26 +1,15 @@
 import * as trpcNext from "@trpc/server/adapters/next";
 import { appRouter } from "@server/routers/_app";
 import { createContext } from "@server/context";
-import { serverEnv } from "@env/server";
-
-export const config = {
-  api: {
-    bodyParser: {
-      // A rough approximation for base64 would be that the size of the data is increased to 4/3 of the original
-      // 10mb avatar limit * 4/3 + additional request data
-      sizeLimit: "14mb",
-    },
-  },
-};
 
 // export API handler
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
-  onError:
-    serverEnv.NODE_ENV === "development"
-      ? ({ path, error }) => {
-          console.error(`❌ tRPC failed on ${path}: ${error}`);
-        }
-      : undefined,
+  onError: ({ path, error }) => {
+    console.error(`❌ tRPC failed on ${path}: ${error}`);
+  },
+  batching: {
+    enabled: true,
+  },
 });
