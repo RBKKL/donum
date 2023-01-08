339b439c158949c3235a3089dda6658c00f08380
Alexandr Baranov <baranovau216@gmail.com>
2023-01-09 03:10:37 +0700

    wip

--- apps/widget/src/event-alert.tsx
+++ apps/widget/src/event-alert.tsx
@@ -24,22 +24,28 @@ export const EventAlert: Component<EventAlertProps> = (props) => {
         {formatTokenAmount(props.event.amount)} ETH
       </h1>
       <p class="event__message">{props.event.message}</p>
-      <p class="event__award">here is award</p>
-      <p class="event__status" style={{ color: "rgb(181,255,35)" }}>
-        {" "}
-        here completed status
-      </p>
-      <p class="event__status" style={{ color: "rgb(220,20,60)" }}>
-        {" "}
-        here failed status
-      </p>
       {"award" in props.event && props.event?.award && (
         <>
-          <p class="event__title">
+          <p class="event__award">
             {" "}
             challenge award: {formatTokenAmount(props.event.award)} ETH
           </p>
-          <p class="event__title"> {props.event.status} </p>
+          {props.event.status === "proposal" ? (
+            <p class="event__status" style={{ color: "rgb(234 179 8)" }}>
+              {" "}
+              New challenge!
+            </p>
+          ) : props.event.status === "complete" ? (
+            <p class="event__status" style={{ color: "rgb(181,255,35)" }}>
+              {" "}
+              Challenge completed!
+            </p>
+          ) : (
+            <p class="event__status" style={{ color: "rgb(220,20,60)" }}>
+              {" "}
+              Challenge failed!
+            </p>
+          )}
         </>
       )}
     </div>
