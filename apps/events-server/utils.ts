393c62b6607848b4ee9540a8b00c960c491365fe
Strafred <bred7355608@gmail.com>
2023-01-08 21:59:41 +0300

    add propose challenge event on event server

--- apps/events-server/utils.ts
+++ apps/events-server/utils.ts
@@ -1,4 +1,7 @@
-import { NewDonationEventObject } from "@donum/contracts/types/DonationsStore";
+import {
+  ChallengeProposedEventObject,
+  NewDonationEventObject,
+} from "@donum/contracts/types/DonationsStore";
 
 export const toDonationObjectForWidget = (donation: NewDonationEventObject) => {
   return {
@@ -6,5 +9,24 @@ export const toDonationObjectForWidget = (donation: NewDonationEventObject) => {
     nickname: donation.nickname,
     amount: donation.amount.toString(),
     message: donation.message,
+  };
+};
+
+enum ChallengeStatus {
+  PROPOSAL = "proposal",
+  FAIL = "fail",
+  COMPLETE = "complete",
+}
+
+export const toProposalChallengeForWidget = (
+  challenge: ChallengeProposedEventObject
+) => {
+  return {
+    from: challenge.from,
+    nickname: challenge.nickname,
+    amount: challenge.proposalPrice.toString(),
+    message: challenge.terms,
+    award: challenge.award.toString(),
+    status: ChallengeStatus.PROPOSAL,
   };
 };
