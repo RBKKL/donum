a68580c29d29d0b30268d9728a63e469c86bf957
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2023-07-10 10:12:24 +0700

    Update start-events-server.sh

--- start-events-server.sh
+++ start-events-server.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/sh
 
+# this variables are public example, use other values in prod
 EVENTS_SERVER_AUTH_TOKEN=xBf9D3MY2Yp1Jywb DATABASE_URL=postgresql://postgres:DkDVHPeaPHsFvLpO@db.bvizuwjbhqlyqyoggyys.supabase.co:5432/postgres pnpm -F @donum/events-server start > logs.log 2>&1 &
