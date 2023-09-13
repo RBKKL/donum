934c0d5012044d3457f0c775a99b308cca29b10f
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2023-09-13 17:21:36 +0700

    use official xstate package for solid hooks

--- apps/widget/src/hooks/useDonationQueueMachine.ts
+++ apps/widget/src/hooks/useDonationQueueMachine.ts
@@ -1,12 +1,12 @@
-import { useMachine } from "./useMachine";
+import { useMachine } from "@xstate/solid";
 import { donationQueueMachine } from "../queue-machine";
 import { DonationInfoWithMetadata } from "../types";
 
 export const useDonationQueueMachine = () => {
-  const { state, send } = useMachine(donationQueueMachine);
+  const [state, send] = useMachine(donationQueueMachine);
 
   const addToQueue = (donation: DonationInfoWithMetadata) => {
-    send("ADD_TO_QUEUE", { item: donation });
+    send({ type: "ADD_TO_QUEUE", item: donation });
   };
 
   return { state, addToQueue };
