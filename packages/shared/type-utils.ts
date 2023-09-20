551686d9321e989b3962b33dff61a48f6bed20aa
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2023-09-20 12:01:25 +0700

    flatten type created with RemoveUndefinedOrNull

--- packages/shared/type-utils.ts
+++ packages/shared/type-utils.ts
@@ -4,9 +4,9 @@ export type * from "type-fest";
 export type RemoveUndefined<T> = Simplify<Required<T>>;
 
 // type to remove "| undefined" and "| null" from all properties of a type
-export type RemoveUndefinedOrNull<T> = {
+export type RemoveUndefinedOrNull<T> = Simplify<{
   [K in keyof T]-?: NonNullable<T[K]>;
-};
+}>;
 
 // type to make all properties of a type optional except for the ones specified
 export type PartialExcept<T, K extends keyof T> = SetRequired<Partial<T>, K>;
