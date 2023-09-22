ab28f5c3ba23dce0a0605e54fa27d0ac49b02865
Mikhail Kopylov <kopylovmichaelfl@gmail.com>
2023-09-22 09:30:08 +0700

    add better comments to type utils

--- packages/shared/type-utils.ts
+++ packages/shared/type-utils.ts
@@ -1,12 +1,13 @@
 import type { SetRequired, Simplify } from "type-fest";
 export type * from "type-fest";
 
+/** Remove "undefined" from all properties of a type */
 export type RemoveUndefined<T> = Simplify<Required<T>>;
 
-// type to remove "| undefined" and "| null" from all properties of a type
+/** Remove "| undefined" and "| null" from all properties of a type */
 export type RemoveUndefinedOrNull<T> = Simplify<{
   [K in keyof T]-?: NonNullable<T[K]>;
 }>;
 
-// type to make all properties of a type optional except for the ones specified
+/** make all properties of a type optional except for the ones specified */
 export type PartialExcept<T, K extends keyof T> = SetRequired<Partial<T>, K>;
